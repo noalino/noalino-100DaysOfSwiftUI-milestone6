154e79cd27f04c250d9d5e3e5d20e50e8d106b33
Noalino <bgelineau.dev@gmail.com>
2024-03-18 10:04:02 +0100

    Initial Commit

---
+++ RollingDice/RollingDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingDiceApp.swift
+//  RollingDice
+//
+//  Created by Noalino on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RollingDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
