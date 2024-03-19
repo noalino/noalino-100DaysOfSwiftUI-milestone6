be3e9b39040963fb81b3005d46088fe1324aec57
Noalino <bgelineau.dev@gmail.com>
2024-03-19 12:21:18 +0100

    Add rolling history

--- RollingDice/ContentView.swift
+++ RollingDice/ContentView.swift
@@ -9,21 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var number = Int.random(in: 1...6)
+    @State private var numbers = [Int]()
 
     var body: some View {
         VStack {
+            List($numbers, id: \.self) { $num in
+                Image(systemName: "\(num).circle")
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .alignmentGuide(.listRowSeparatorLeading) { d in
+                        d[.leading]
+                    }
+            }
+            .listStyle(.plain)
+
             Text("Number: \(number)")
-            Button("Roll dice", action: roll)
+                .font(.title)
+
+            Button("ROLL", action: roll)
                 .padding()
                 .background(.blue)
                 .foregroundColor(.white)
                 .clipShape(.rect(cornerRadius: 10))
+
         }
-        .padding()
+        .padding(.top, 1)
     }
 
     func roll() {
         number = Int.random(in: 1...6)
+        numbers.append(number)
     }
 }
 
